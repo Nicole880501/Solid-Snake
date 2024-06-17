ae03e1329267d60ca1d76cc1c205666bf3630e11
e055339 <e055339@gmail.com>
2024-06-17 10:01:42 +0800

    Feat: add personalMaxScore and AllplayerRanking api

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -1,1 +1,38 @@
+const { getPersonalRecord, getAllPlayerRecord } = require("../models/record");
 
+exports.getPersonalMaxScore = async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    const userRecord = await getPersonalRecord(username);
+
+    res.status(200).json({
+      data: {
+        name: userRecord.user_name,
+        score: userRecord.score,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getRanking = async (req, res) => {
+  try {
+    const ranking = await getAllPlayerRecord();
+
+    const playerData = [];
+
+    ranking.map((data) => {
+      playerData.push(data);
+    });
+
+    playerData.sort((a, b) => b.score - a.score);
+
+    res.status(200).json({
+      data: playerData,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- models/record.js
+++ models/record.js
@@ -12,6 +12,65 @@ const pool = mysql.createPool({
 
 exports.createRecord = async (data) => {
   const result = pool.query("INSERT INTO Records SET ?", data);
-  console.log(result);
   return result;
+};
+
+exports.getPersonalRecord = async (userName) => {
+  const query = `
+    SELECT 
+        r1.user_name,
+        r1.skin,
+        r1.score,
+        r1.timestamp
+    FROM 
+        Records r1
+    INNER JOIN (
+        SELECT 
+            user_name,
+            MAX(score) as max_score
+        FROM 
+            Records
+        WHERE 
+            user_name = ?
+        GROUP BY 
+            user_name
+    ) r2 ON r1.user_name = r2.user_name AND r1.score = r2.max_score;
+  `;
+
+  try {
+    const [rows] = await pool.query(query, [userName]);
+    return rows[0];
+  } catch (error) {
+    console.error("Error fetching highest score:", error);
+    throw error;
+  }
+};
+
+exports.getAllPlayerRecord = async () => {
+  const query = `
+  SELECT 
+      r1.user_name,
+      r1.skin,
+      r1.score,
+      r1.timestamp
+  FROM 
+      Records r1
+  INNER JOIN (
+      SELECT 
+          user_name,
+          MAX(score) as max_score
+      FROM 
+          Records
+      GROUP BY 
+          user_name
+  ) r2 ON r1.user_name = r2.user_name AND r1.score = r2.max_score;
+`;
+
+  try {
+    const [rows] = await pool.query(query);
+    return rows;
+  } catch (error) {
+    console.error("Error fetching highest score:", error);
+    throw error;
+  }
 };
